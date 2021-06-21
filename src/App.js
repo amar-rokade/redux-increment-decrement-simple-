32b7026e51c47f04387f56091880490b0fcb7be2
amar-rokade <rokade6117@gmail.com>
2021-06-21 18:15:49 +0530

    redux increment and decrement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions/index";
 
 function App() {
+  const myState = useSelector((state) => state.changeNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        style={{
+          alignItems: "center",
+          alignContent: "center",
+        }}
+        className="App"
+      >
+        <h1
+          style={{
+            fontWeight: "bolder",
+            color: "pink",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Increament and Decrement
+        </h1>
+        <div style={{ display: "inline-flex", marginTop: 50 }}>
+          <div onClick={() => dispatch(decNumber())}>
+            <button
+              style={{ fontSize: "10rem", marginRight: 30, cursor: "pointer" }}
+            >
+              -
+            </button>
+          </div>
+
+          <div style={{ fontSize: "8rem", marginRight: 30 }}>
+            {/* <input value={myState} /> */}
+            {myState}
+          </div>
+          <div onClick={() => dispatch(incNumber())}>
+            <button
+              style={{
+                fontSize: "10rem",
+                cursor: "pointer",
+              }}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
